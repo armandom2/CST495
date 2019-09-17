521e4dfb9ac1af322c004a9eda4ef82de7d3dc72
armandomiguel <armando.miguel518@gmail.com>
2019-09-17 16:25:41 -0700

    Added the tumblr part 1 assignment

---
+++ tumblr/tumblr/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  tumblr
+//
+//  Created by Armando Miguel on 9/16/19.
+//  Copyright © 2019 Armando Miguel. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
